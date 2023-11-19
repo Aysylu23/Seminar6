bbc248c9d8999883996766112c6706015a71fb2b
Aysylu <aysylu_bad@mail.ru>
2023-11-19 21:38:37 +0500

    add task2

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*40. Напишите программу, которая принимает на вход три числа и проверяет, 
+может ли существовать треугольник с сторонами такой длины.*/
+
+
+int fistSide = ReadInt("Введите первое число: ");
+int secondSide = ReadInt("Введите первое число: ");
+int thirdSide = ReadInt("Введите первое число: ");
+
+if (fistSide + secondSide > thirdSide && fistSide + thirdSide > secondSide && secondSide + thirdSide > fistSide)
+{
+    Console.WriteLine("Три числа являются сторонами треугольника");
+}
+else
+{
+    Console.WriteLine("Три числа НЕ являются сторонами треугольника");
+}
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
