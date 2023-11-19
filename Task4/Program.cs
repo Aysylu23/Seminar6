0c0768010d36841f37b8be2b0d3f9e329b9c9b00
Aysylu <aysylu_bad@mail.ru>
2023-11-19 21:39:16 +0500

    add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,30 @@
+﻿// Не используя рекурсию, выведите первые N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+int n = ReadInt("Введите число: ");
+int[] fibonacci = new int[n];
+if(n > 0)
+    fibonacci[0] = 0;
+if(n > 1)
+    fibonacci[1] = 1;
+
+for(int i = 2; i < fibonacci.Length; i++)
+{
+    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+}
+PrintIntArray(fibonacci);
+
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+void PrintIntArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+
+    Console.WriteLine();
+}
